706e83fe20dc1372f1f30995c3b65ecf9a1cec25
Dior.Shelton <dior.shelton@gmail.com>
2021-06-07 12:41:44 -0500

    Complete meal.js test

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,51 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  var menuItemName = `Delicious ${name}`;
+  return menuItemName;
+};
 
+function createMenuItem(name, price, type) {
+  var menuItem = {
+    name:name,
+    price:price,
+    type:type
+  };
+  return menuItem;
+};
+
+function addIngredients(ingredient
+  , list) {
+  if (!list.includes(ingredient)) {
+    list.push(ingredient)}
+  return list;
+};
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`;
+};
+
+function decreasePrice(menuItem) {
+  var discountAmount = (menuItem * .10);
+  var discountedPrice = (menuItem - discountAmount);
+  return discountedPrice;
+};
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  };
+  return recipe;
 }
-
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
+ 
